6d05c949cbfba0ebe80c0c5c62967e94a3e70786
David Wagnon <david.wagnon02@gmail.com>
2021-07-18 17:51:07 -0500

    Deleted catagories table , model, dao files

--- src/main/java/com/codeup/adlister/controllers/ViewIndividualServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewIndividualServlet.java
@@ -2,9 +2,6 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
-import com.codeup.adlister.models.Category;
-import com.codeup.adlister.models.User;
-import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -3,7 +3,6 @@ package com.codeup.adlister.dao;
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
-    private static Categories categoriesDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -20,11 +19,5 @@ public class DaoFactory {
         return usersDao;
     }
 
-    public static Categories getCategoriesDao() {
-        if (categoriesDao == null) {
-            categoriesDao = new MySQLCategoriesDao(config);
-        }
-        return categoriesDao;
-    }
 
 }
